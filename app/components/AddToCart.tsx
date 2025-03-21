075f7028b4c7ff4f8f6d13547a9530b578db1133
deepsth <infodipstha@gmail.com>
2025-03-21 17:14:10 +0545

    added route and tested get and post request method

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-       <button className="btn btn-primary" onClick={() => console.log('clicked Add to cart')}>Add to Cart</button>
+       <button className="bg-blue-500 btn btn-primary" onClick={() => console.log('clicked Add to cart')}>Add to Cart</button>
     </div>
   )
 }
