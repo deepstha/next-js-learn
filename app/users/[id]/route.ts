d9803c78ab98eaaf1d3d795edc736f0554d8b9cd
deepsth <infodipstha@gmail.com>
2025-04-04 15:37:42 +0545

    adding user id folder

---
+++ app/users/[id]/route.ts
@@ -0,0 +1,10 @@
+
+
+export async function GET( 
+    request : Request, 
+    { params }: { params: {id: string}}
+) {
+    const { id } = await params;
+    // const  user = users.find((user => user))
+    
+}
